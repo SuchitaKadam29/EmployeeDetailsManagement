6cfb2ff3a4a4734c42be19310265f2f793cc6275
SuchitaKadam29 <128416239+SuchitaKadam29@users.noreply.github.com>
2023-04-28 11:27:38 +0530

    Update Employee.java

--- src/main/java/com/hdfc/capstone/entity/Employee.java
+++ src/main/java/com/hdfc/capstone/entity/Employee.java
@@ -1,17 +1,11 @@
 package com.hdfc.capstone.entity;
 
+
 import java.sql.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Positive;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -33,18 +27,12 @@ import lombok.Setter;
 public class Employee {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Positive(message = "Employee ID must be a positive integer")
-	@NotBlank(message = "Employee Id is required")
+//	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int employeeId;
 
-	@NotBlank(message = "Employee name is required")
 	private String employeeName;
 
-	@NotNull(message = "Date of birth is required")
 	private Date dateOfBirth;
 
 }
-
-
 
